048f1f10193125723db961881354f7d7cada55d0
eugene.vashchilin <77799844+V3ugen@users.noreply.github.com>
2022-07-18 19:25:12 +0300

    task-02_DOne

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+// ingredients.forEach(function callback(element) {
+//   const newLi = document.createElement('li');
+//   newLi.textContent = element;
+//   ingredientsEl.append(newLi)
+// })
+
+const elements = ingredients.map(option => {
+  const newLi = document.createElement('li');
+  newLi.textContent = option;
+  return newLi;
+}) ;
+console.log(...elements );
+ingredientsEl.append(...elements)
