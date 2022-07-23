0a486d71afd62cece11283b7b452639b3ad573e2
eugene.vashchilin <77799844+V3ugen@users.noreply.github.com>
2022-07-23 22:47:43 +0300

    up 9

--- js/task-09.js
+++ js/task-09.js
@@ -4,18 +4,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
 const changeColorBtnEl = document.querySelector('.change-color');
-
 const colorEl = document.querySelector('.color');
-
 const bodyEl = document.querySelector('body');
 
 
 function changeColor() {
-
-  bodyEl.style.backgroundColor = getRandomHexColor()
-
+  bodyEl.style.backgroundColor = getRandomHexColor();
   colorEl.textContent = bodyEl.style.backgroundColor;
 }
-changeColorBtnEl.addEventListener("click",changeColor)
+changeColorBtnEl.addEventListener("click", changeColor)
+
+
+// const btnEl = document.querySelector("button");
+// const spanContentEl = document.querySelector("span");
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// function changeBodyBgrColor(event) {
+//   const getRandomColorValue = getRandomHexColor();
+//   document.body.style.backgroundColor = getRandomColorValue;
+//   spanContentEl.textContent = getRandomColorValue;
+// }
+
+// btnEl.addEventListener("click", changeBodyBgrColor);
